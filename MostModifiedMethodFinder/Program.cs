cd6a59a830df6ee82ac4703938910ec3e0fd4231
Uzair Shamim <Uzair.Shamim@D2L.com>
2024-03-04 09:48:32 -0500

    Implement

--- MostModifiedMethodFinder/Program.cs
+++ MostModifiedMethodFinder/Program.cs
@@ -1,43 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Configuration;
 using MostModifiedMethodFinder;
 
 Console.WriteLine("Hello, World!");
 
+IConfigurationBuilder builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-string filePath = "TwitchEverywhereCLI/TwitchConnection.cs";
-string repoPath = "C:\\Users\\uzi\\RiderProjects\\TwitchEverywhere";
+IConfiguration config = builder.Build();
 
-CommitFinder commitFinder = new( repoPath );
+string? repoPath = config["RepositoryPath"];
+string? filePath = config["FilePath"];
+
+if (string.IsNullOrEmpty(repoPath) || string.IsNullOrEmpty(filePath)) {
+    Console.WriteLine("Please provide a repository path and a file path in the appsettings.json file.");
+    return;
+}
+
+CommitFinder commitFinder = new(repoPath);
 RoslynIntegration roslynIntegration = new();
 Dictionary<string, long> methodChanges = new();
 
 List<string> commitShas = commitFinder.GetCommitShas(filePath);
 
-for (int i = 0; i < commitShas.Count - 1; i++)
-{
-    var oldContent = commitFinder.GetFileContentAtCommit(filePath, commitShas[i]);
-    var newContent = commitFinder.GetFileContentAtCommit(filePath, commitShas[i + 1]);
+for (int i = 0; i < commitShas.Count - 1; i++) {
+    string oldContent = await commitFinder.GetFileContentAtCommitAsync(filePath, commitShas[i]);
+    string newContent = await commitFinder.GetFileContentAtCommitAsync(filePath, commitShas[i + 1]);
     
-    var oldTree = roslynIntegration.ParseToSyntaxTree(oldContent);
-    var newTree = roslynIntegration.ParseToSyntaxTree(newContent);
+    SyntaxTree oldTree = roslynIntegration.ParseToSyntaxTree(oldContent);
+    SyntaxTree newTree = roslynIntegration.ParseToSyntaxTree(newContent);
     
-    var changedMethods = roslynIntegration.GetDetailedChanges(oldTree, newTree);
+    IEnumerable<string> changedMethods = roslynIntegration.GetDetailedChanges(oldTree, newTree);
     
-    foreach (var method in changedMethods)
-    {
+    foreach (string method in changedMethods) {
         Console.WriteLine($"Changed method: {method} between commits {commitShas[i]} and {commitShas[i + 1]}");
-        if (methodChanges.ContainsKey(method))
-        {
+        if (methodChanges.ContainsKey(method)) {
             methodChanges[method]++;
-        }
-        else
-        {
+        } else {
             methodChanges[method] = 1;
         }
     }
 }
 
-foreach (var methodChange in methodChanges) {
+foreach (var methodChange in methodChanges.OrderBy(m => m.Value)) {
     Console.WriteLine($"{methodChange.Key}: {methodChange.Value}");
 }
